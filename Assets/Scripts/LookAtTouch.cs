e7971467e824456fd2d290ad2ad0402f2a6f8b56
zhouzihong <1635757679@qq.com>
2023-12-27 21:26:27 +0800

    开始调参

--- Assets/Scripts/LookAtTouch.cs
+++ Assets/Scripts/LookAtTouch.cs
@@ -4,8 +4,6 @@ public class LookAtTouch : MonoBehaviour
 {
     public Camera mainCamera;
     public Transform headTransform;
-    public int a;
-    public int b;
 
     void Update()
     {
@@ -13,19 +11,14 @@ public class LookAtTouch : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
 
-            Debug.Log("触摸位置: " + touch.position);
-
-            Vector3 touchPosition = mainCamera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, mainCamera.nearClipPlane));
-
-            Debug.Log("世界坐标位置: " + touchPosition);
-
-            Vector3 directionToLook = touchPosition - headTransform.position;
-
-            Debug.Log("朝向方向: " + directionToLook);
-
-            headTransform.rotation = Quaternion.LookRotation(directionToLook);
-
-            Debug.Log("头部旋转: " + headTransform.rotation);
+            // 将触摸点从屏幕坐标转换为世界坐标
+            Vector3 touchPosition = new Vector3(touch.position.x, touch.position.y, 10f);
+            touchPosition.x =0.96f;
+            touchPosition.y =0f;
+            touchPosition.z =10f;
+            // 让角色的头部面向触摸点
+            Debug.Log("转换后的眼睛位置：" + touchPosition);
+            headTransform.LookAt(touchPosition);
         }
     }
 }
